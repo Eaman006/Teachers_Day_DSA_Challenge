8377ad78fac6bd13246e035fc5675c3ca6010a0e
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-27 10:13:56 +0530

    modification in Valid palindrome

--- Valid Palindrome/src/Solution.java
+++ Valid Palindrome/src/Solution.java
@@ -3,6 +3,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-                
+        String a = s.replace(" ","");
+        StringBuilder r = new StringBuilder();
+        
+
     }
 }
