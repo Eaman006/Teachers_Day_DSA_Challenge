7d3f2438828176b72553ffb1da0d06fb24d3763f
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-28 16:32:09 +0530

    Implement fizzBuzz method and update main method for user input handling

--- Fizz Buzz/src/App.java
+++ Fizz Buzz/src/App.java
@@ -1,10 +1,17 @@
+import java.util.*;
 class Solution {
     public List<String> fizzBuzz(int n) {
         
+
     }
 }
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution s =new Solution();
+        s.fizzBuzz(n);
+        
+        
     }
 }
