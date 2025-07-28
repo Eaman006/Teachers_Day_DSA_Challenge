674800628538121ba0e60c04f1642bcaf53fece0
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-28 17:06:32 +0530

    Implement fizzBuzz method and update App class for output display

--- Fizz Buzz/src/App.java
+++ Fizz Buzz/src/App.java
@@ -1,6 +1,26 @@
 import java.util.*;
 class Solution {
     public List<String> fizzBuzz(int n) {
+        List<String> r = new ArrayList<>();
+        for(int i=1; i<=n; i++ ){
+            if (i%3==0 && i%5!=0) {
+                r.add("Fizz");
+                
+            }
+            if (i%5==0 && i%3!=0) {
+                r.add("Buzz");
+                
+            }
+            if (i%3==0 && i%5==0) {
+                r.add("FizzBuzz");
+                
+            }
+            if(i%3!=0 && i%5!=0){
+                r.add(Integer.toString(i));
+            }
+        }
+        return r;
+
         
 
     }
@@ -10,8 +30,9 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Solution s =new Solution();
-        s.fizzBuzz(n);
         
-        
+        System.out.println(s.fizzBuzz(n));      
+        sc.close();
     }
+    
 }
