292b35dd44e4c0189b006a2e1258c997f429ed50
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-28 16:21:34 +0530

    Add Solution class with fizzBuzz method skeleton

--- Fizz Buzz/src/App.java
+++ Fizz Buzz/src/App.java
@@ -1,3 +1,8 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+    }
+}
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
