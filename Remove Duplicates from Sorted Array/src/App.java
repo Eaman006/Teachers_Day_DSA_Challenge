4da5e5749ac2adca7dec04111324a1d3b8468010
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-30 14:11:39 +0530

    Implement removeDuplicates method in Solution class and update App class for user input handling

--- Remove Duplicates from Sorted Array/src/App.java
+++ Remove Duplicates from Sorted Array/src/App.java
@@ -1,5 +1,28 @@
+import java.util.*;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]==nums[i+1]) {
+                nums[i+1]=nums[i+2];                               
+            }
+            
+        }
+        return nums.length;
+        
+    }
+}
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner n= new Scanner(System.in);
+        String a = n.nextLine();
+        a=a.substring(1,a.length()-1);
+        String [] b =a.split(",");
+        int[] c = new int[b.length];
+        for(int i=0; i<c.length;i++){
+            c[i]=Integer.parseInt(b[i]);
+        }
+        Solution s = new Solution();
+        System.out.println(s.removeDuplicates(c));
+        
     }
 }
