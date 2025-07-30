5398143952d3c783c65107c0ec2e8766383db2ba
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-30 13:24:41 +0530

    Refactor App class to implement user input handling and call numIdenticalPairs method

--- Number of Good Pairs/src/App.java
+++ Number of Good Pairs/src/App.java
@@ -1,10 +1,20 @@
+import java.util.*;
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         
     }
 }
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        a=a.substring(1, a.length()-1);
+        String [] b=a.split(",");
+        int[] nums = new int[b.length];
+        for(int i = 0; i<nums.length;i++){
+            nums[i] = Integer.parseInt(b[i]);
+        }
+        Solution s = new Solution();
+        System.out.println(s.numIdenticalPairs(nums));                
     }
 }
