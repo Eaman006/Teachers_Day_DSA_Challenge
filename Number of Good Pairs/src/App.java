63ae9d60e3110b229b5a3f3097ccfb0995a5b361
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-30 08:44:33 +0530

    Add initial project configuration and implement Solution class with numIdenticalPairs method skeleton

---
+++ Number of Good Pairs/src/App.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+    }
+}
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
