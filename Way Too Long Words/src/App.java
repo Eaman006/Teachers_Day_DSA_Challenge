c69cddb739c53d13643c2472cb8b11fc2e168185
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-01 20:25:54 +0530

    Implement main logic for processing long words in App class

--- Way Too Long Words/src/App.java
+++ Way Too Long Words/src/App.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] s = new String[n];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = sc.nextLine();
+        }
+        sc.close();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].length() > 10) {
+                System.out.print(s[i].charAt(0));
+                System.out.print(s[i].length() - 2);
+                System.out.print(s[i].charAt(s[i].length() - 1));
+                System.out.println("");
+
+            } 
+            else {
+                System.out.println(s[i]);
+            }
+
+        }
     }
 }
