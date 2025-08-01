5baeea4744f88c1df8105aeaf378a96d4438af02
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-01 21:38:18 +0530

    Implement main logic for processing input string and formatting output

--- String Task/src/App.java
+++ String Task/src/App.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        s=s.toLowerCase();
+        s=s.replaceAll("[aeiouy]", "");
+        String [] a=s.split("");
+        s= String.join(".",a);
+        System.out.println("."+s);    
     }
 }
