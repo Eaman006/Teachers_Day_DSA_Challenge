134d94a8b7c43f707f2f352b585625be762cd050
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-03 18:14:38 +0530

    Implement main logic for comparing two input strings and outputting the result

--- Petya and Strings/src/App.java
+++ Petya and Strings/src/App.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        sc.close();
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        if (s1.equals(s2)) {
+            System.out.println("0");
+        }
+        else if (s1.compareTo(s2)>0) {
+            System.out.println("1");
+            
+        }
+        else if (s1.compareTo(s2)<0) {
+            System.out.println("-1");
+            
+        }
     }
 }
