07cd6804d6908018df4e1a01b0833d164e1358c0
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-06 16:11:08 +0530

    Implement main logic for unique character counting and outputting result in App class

--- Boy or Girl/src/App.java
+++ Boy or Girl/src/App.java
@@ -1,5 +1,21 @@
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        s.close();
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i<a.length(); i++){
+            char c = a.charAt(i);
+            if (sb.indexOf(String.valueOf(c))==-1) {
+                sb.append(c);                
+            }
+        }
+        if (sb.length()%2==0) {
+            System.out.println("CHAT WITH HER!");            
+        }
+        else{
+            System.out.println("IGNORE HIM!");
+        }
     }
 }
