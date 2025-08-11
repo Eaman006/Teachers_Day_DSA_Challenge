a8c8bc29d206008e16ff8b5539c1450a96958824
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-11 14:15:06 +0530

    Implement main method in App class to read input and initialize character processing logic

--- Word/src/App.java
+++ Word/src/App.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        for(int i = 0; i<a.length(); i++){
+            if (a.charAt(i)) {
+                
+            }
+        }
+
     }
 }
