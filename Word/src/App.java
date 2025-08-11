00c9a3a04d5a7a07b962310d5110cb7c151cefe7
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-11 17:55:43 +0530

    Refactor App class to include character counting logic and output formatting based on case analysis

--- Word/src/App.java
+++ Word/src/App.java
@@ -3,10 +3,24 @@ public class App {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         String a = s.nextLine();
+        s.close();
+        a=a.trim();
+        int count=0;
+        int c=0;
         for(int i = 0; i<a.length(); i++){
-            if (a.charAt(i)) {
-                
+            if (Character.isUpperCase(a.charAt(i))) {
+                count++;
+                                
             }
+            else{
+                c++;
+            }
+        }
+        if (count>c) {
+            System.out.println(a.toUpperCase());            
+        }
+        else{
+            System.out.println(a.toLowerCase());
         }
 
     }
