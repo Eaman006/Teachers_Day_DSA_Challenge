eedb52745a8389d2882a672b690f61585a785f83
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-13 15:09:45 +0530

    Add initial implementation of App class and VS Code settings

---
+++ Bit++/src/App.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x=0;
+        sc.nextLine();
+        for(int i =0; i<n; i++){
+            String s = sc.nextLine();
+            switch (s) {
+                case "--x":
+                                    
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+        }
+    }
+}
