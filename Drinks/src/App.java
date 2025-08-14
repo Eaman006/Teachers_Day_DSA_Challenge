11348c25bb9a94bff72078b5947a4b89c892889d
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-14 21:34:38 +0530

    Add implementation of main method for input processing and average calculation

--- Drinks/src/App.java
+++ Drinks/src/App.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class App {
     public static void main(String[] args){
-        
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int a=0;
+        for(int i=0;i<n;i++){
+            int b = sc.nextInt();
+            a=a+b;
+        }
+        sc.close();
+        float b=(float)a/100;
+        float r=((float)b/n)*100;
+        System.out.println(r);
+
     }
 }
