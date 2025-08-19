d72a7adf68bd6388158f8ad260812e899b0c432c
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-19 12:11:33 +0530

    Add initial implementation of App class with input handling and project settings

---
+++ Petya and Countryside/src/App.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int c=1;
+        int [] a = new int[n];
+        for(int i=0;i<n; i++){
+            a[i]=sc.nextInt();            
+        }
+        sc.close();
+        for(int i=0;i<a.length-1;i++){
+            for(int j=1; j)
+        }
+        
+    }
+}
