b38c36ccb0f7258a5c9190cdb8d06bb60cfa3453
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-24 15:07:13 +0530

    Refactor App class to implement longest non-decreasing subsequence logic and streamline input handling

--- Petya and Countryside/src/App.java
+++ Petya and Countryside/src/App.java
@@ -4,15 +4,36 @@ public class App {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        int c=1;
-        int [] a = new int[n];
-        for(int i=0;i<n; i++){
-            a[i]=sc.nextInt();            
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
         sc.close();
-        for(int i=0;i<a.length-1;i++){
-            for(int j=1; j)
+        int[] L = new int[n];
+        L[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i - 1] <= a[i]){
+                 L[i] = L[i - 1] + 1;
+            }
+            else{
+                 L[i] = 1;
+            }
         }
+        int[] R = new int[n];
+        R[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i + 1] <= a[i]){
+                R[i] = R[i + 1] + 1;
+            }
+            else{
+                R[i] = 1;
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, L[i] + R[i] - 1);
+        }
+        System.out.println(max);
         
     }
 }
