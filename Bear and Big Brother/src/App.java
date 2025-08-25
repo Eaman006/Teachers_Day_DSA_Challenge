89f1100e1faff8e3dd63ad6fb51d1a202ba925ae
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-08-25 20:01:06 +0530

    Add initial implementation of App class with project settings and README documentation

---
+++ Bear and Big Brother/src/App.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int i = 0;
+        while (a<=b) {
+            a=3*a;
+            b=2*b;
+            i=i+1;
+        }
+        System.out.println(i);
+    }
+}
