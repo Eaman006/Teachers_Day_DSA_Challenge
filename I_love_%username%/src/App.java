490ab85e17a8cf83e66fa2754ccf9018b8a3384c
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-09-03 19:13:24 +0530

    Add initial implementation of App class with input handling and project settings

---
+++ I_love_%username%/src/App.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[]= new int[n];
+        int c=0;
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+        for(int i = 0; i<n; i++){
+            
+        }
+        System.out.println(c);
+    }
+}
