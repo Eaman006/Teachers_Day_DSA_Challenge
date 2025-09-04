36225ffa0cf247d217e9e92888092b5e44646ca2
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-09-04 17:38:36 +0530

    Refactor input handling and counting logic in App class

--- I_love_%username%/src/App.java
+++ I_love_%username%/src/App.java
@@ -3,19 +3,29 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int a[]= new int[n];
-        int c=0;
-        for(int i=0; i<n; i++){
-            a[i]=sc.nextInt();
+        sc.nextLine();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
         sc.close();
-        int b=a[0];
-        int w=0;
-        for(int i = 0; i<n; i++){
-            if (a[i]) {
-                
+
+        int count = 0;
+        if (n > 0) {
+            int min = a[0];
+            int max = a[0];
+            for (int i = 1; i < n; i++) {
+                if (a[i] > max) {
+                    count++;
+                    max = a[i];
+                }
+                if (a[i] < min) {
+                    count++;
+                    min = a[i];
+                }
             }
         }
-        System.out.println(c);
+
+        System.out.println(count);
     }
 }
