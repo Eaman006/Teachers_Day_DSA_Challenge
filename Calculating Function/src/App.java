fcd65527ad4b86ed497fdfa4bd7136aa15df02ac
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-09-04 20:10:46 +0530

    Implement main method for App class to calculate alternating sum based on user input

--- Calculating Function/src/App.java
+++ Calculating Function/src/App.java
@@ -1,5 +1,19 @@
+import java.util.*;;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        sc.close();
+        int a = 0;
+        for(int i = 1; i<=n; i++){
+            if (i%2!=0) {
+                a=a-i;                
+            } 
+            else{
+                a=a+i;
+            }           
+        }
+        System.out.println(a);
     }
 }
