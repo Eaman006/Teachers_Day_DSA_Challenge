c347121605720bd031cd7fc06582c46b85dfd37d
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-09-04 20:33:44 +0530

    Refactor main method in App class to calculate alternating sum using long data type

--- Calculating Function/src/App.java
+++ Calculating Function/src/App.java
@@ -2,17 +2,16 @@ import java.util.*;;
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         sc.nextLine();
         sc.close();
-        int a = 0;
-        for(int i = 1; i<=n; i++){
-            if (i%2!=0) {
-                a=a-i;                
-            } 
-            else{
-                a=a+i;
-            }           
+        long a = 0;
+        long b=n-1;
+        if(n%2==0){
+            a=n/2;
+        }
+        else{
+            a=b/2-n;
         }
         System.out.println(a);
     }
